7052b634164d87de3de79d737e309a75d9a890e1
Jacob Aust <jaust.413@gmail.com>
2025-03-21 20:56:29 -0700

    Initial project commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from openai import OpenAI
+from flask import Flask, render_template, request
+from dotenv import dotenv_values
+import json
+
+config = dotenv_values(".env")
+client = OpenAI(api_key=config["OPEN_AI_API_KEY"])
+
+app = Flask(__name__, template_folder="templates", static_url_path="", static_folder="static")
+
+
+def get_colors(msg):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        store=False,
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                You are a color palette generating assistant that responds to text prompts for color palettes.
+                You should generate color palettes that fit the theme, mood, or instructions in the prompt.
+                The palettes should be between 2 and 8 colors.
+        
+                Q: Google brand colors
+                A: ["#4285F4", "#34A853", "#FFC107", "#FF5959"]
+        
+                Desired Format: a JSON array of hexadecimal color codes
+                """,
+            },
+            {
+                "role": "user", 
+                "content": f"Convert the following verbal description of a color palette into a list of colors: {msg}"
+            },
+        ],
+    )
+
+
+    colors = json.loads(completion.choices[0].message.content)
+    return colors
+
+
+
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
+    query = request.form.get("query")
+    colors = get_colors(query)
+    return {"colors": colors}
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
